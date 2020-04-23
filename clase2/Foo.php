731ee750442dc6c9fb06346fe506e8e614de9d58
marcos pinardi <exegeses@gmail.com>
2020-04-23 10:28:43 -0300

    visibilidad de los metodos

---
+++ clase2/Foo.php
@@ -0,0 +1,35 @@
+<?php
+
+    class Foo
+    {
+        public function metodoPublico()
+        {
+            echo 'método público <br>';
+        }
+        private function metodoPrivado()
+        {
+            echo 'método privado <br>';
+        }
+        public function auxiliar()
+        {
+            echo 'método auxiliar <br>';
+            $this->metodoPrivado();
+        }
+        static function metodoEstatico()
+        {
+            echo 'método estático <br>';
+        }
+
+    }
+
+    #### en otro archivo se instancia
+    #### y luego se invocan métodos
+
+    $objFoo = new Foo;
+    $objFoo->metodoPublico();
+    //$objFoo->metodoPrivado(); ## no se puede porque es privado
+    $objFoo->auxiliar(); ## invoca a un método privado
+
+    echo '<hr>';
+
+    Foo::metodoEstatico();
