3b9feb0d5ed2cfebad2ba6025cf7db8c7aa14a2d
marcos pinardi <exegeses@gmail.com>
2020-04-30 12:44:41 -0300

    class Destino

---
+++ agencia/clases/Destino.php
@@ -0,0 +1,129 @@
+<?php
+
+    class Destino
+    {
+        private $destID;
+        private $destNombre;
+        private $regID;
+        private $destPrecio;
+        private $destAsientos;
+        private $destDisponible;
+        private $destActivo;
+
+        ######################################
+        ####  getters & setters ####
+
+        /**
+         * @return mixed
+         */
+        public function getDestID()
+        {
+            return $this->destID;
+        }
+
+        /**
+         * @param mixed $destID
+         */
+        public function setDestID($destID)
+        {
+            $this->destID = $destID;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestNombre()
+        {
+            return $this->destNombre;
+        }
+
+        /**
+         * @param mixed $destNombre
+         */
+        public function setDestNombre($destNombre)
+        {
+            $this->destNombre = $destNombre;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getRegID()
+        {
+            return $this->regID;
+        }
+
+        /**
+         * @param mixed $regID
+         */
+        public function setRegID($regID)
+        {
+            $this->regID = $regID;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestPrecio()
+        {
+            return $this->destPrecio;
+        }
+
+        /**
+         * @param mixed $destPrecio
+         */
+        public function setDestPrecio($destPrecio)
+        {
+            $this->destPrecio = $destPrecio;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestAsientos()
+        {
+            return $this->destAsientos;
+        }
+
+        /**
+         * @param mixed $destAsientos
+         */
+        public function setDestAsientos($destAsientos)
+        {
+            $this->destAsientos = $destAsientos;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestDisponible()
+        {
+            return $this->destDisponible;
+        }
+
+        /**
+         * @param mixed $destDisponible
+         */
+        public function setDestDisponible($destDisponible)
+        {
+            $this->destDisponible = $destDisponible;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestActivo()
+        {
+            return $this->destActivo;
+        }
+
+        /**
+         * @param mixed $destActivo
+         */
+        public function setDestActivo($destActivo)
+        {
+            $this->destActivo = $destActivo;
+        }
+
+
+    }
--- agencia/clases/Region.php
+++ agencia/clases/Region.php
@@ -40,7 +40,7 @@
 
     ######################################
     ####  getters & setters ####
-        #
+
         public function getRegID()
         {
             return $this->regID;
